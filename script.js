6eb38ee446d71ce52e15841f2971573829c816bb
Amar Patra <amarpatra932@gmail.com>
2026-01-31 06:57:25 +0530

    Today done few items

--- script.js
+++ script.js
@@ -13,48 +13,59 @@
 //Timeline
 
 //Loader Hide
+function loadingAnimation() {
+  let tl = gsap.timeline();
+  //loder
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.3,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1", {
+    opacity: 0,
+    // when start then it execute
+    onStart: function () {
+      let h5Timer = document.querySelector("#line1-part1 h5");
+      let grow = 0;
+      let timer = setInterval(() => {
+        if (grow < 100) {
+          grow++;
+          h5Timer.textContent = grow;
+        } else {
+          h5Timer.textContent = grow;
+        }
+      }, 35);
+    },
+  });
+  tl.to(".line h2", {
+    animationName: "nowAnim",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 3.8,
+  });
 
-let tl = gsap.timeline();
-//loder
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.3,
-  duration: 0.6,
-  delay: 0.5,
-});
-tl.from("#line1-part1", {
-  opacity: 0,
-  // when start then it execute
-  onStart: function () {
-    let h5Timer = document.querySelector("#line1-part1 h5");
-    let grow = 0;
-    let timer = setInterval(() => {
-      if (grow < 100) {
-        grow++;
-        h5Timer.textContent = grow;
-      } else {
-        h5Timer.textContent = grow;
-      }
-    }, 35);
-  },
-});
-tl.to(".line h2", {
-  animationName: "nowAnim",
-  opacity: 1,
-});
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 3.8,
-});
+  tl.from("#page1", {
+    y: 1600,
+    duration: 0.5,
+    delay: 0.2,
+    opacity: 0,
+  });
 
-tl.from("#page1", {
-  y: 1600,
-  duration: 0.5,
-  delay: 0.2,
-  opacity: 0,
-});
+  tl.to("#loader", {
+    display: "none",
+  });
+}
 
-tl.to("#loader", {
-  display: "none",
+loadingAnimation();
+
+document.addEventListener("mousemove", function (dets) {
+  console.log(dets);
+  gsap.to("#crsr", {
+    left: dets.x,
+    top: dets.y,
+  });
 });
